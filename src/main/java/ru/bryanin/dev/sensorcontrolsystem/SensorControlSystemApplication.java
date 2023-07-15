0817595b68ad2805dceb00cbe535b7125909ac5e
Bryanin Dmitriy <javadeviloper@gmail.com>
2023-07-15 18:26:55 +0300

    Init commit

---
+++ src/main/java/ru/bryanin/dev/sensorcontrolsystem/SensorControlSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.bryanin.dev.sensorcontrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bryanin/dev/sensorcontrolsystem/SensorControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bryanin.dev.sensorcontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
